ff0c4799e3daa596fb75459e76687a9331860b4f
David French <David@mefalf.net>
2020-12-06 21:42:52 -0600

    adding prompt to js file

--- server.js
+++ server.js
@@ -11,3 +11,69 @@ function init() {
 
     prompts();
 }
+
+const { choice } = prompt([
+    {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do",
+        choices: [
+            {
+                name: "View All Employees",
+                value: "View_Employee"
+            },
+            {
+                name: "View All Employees By Department",
+                value: "View_Employee_By_Department"
+            },
+            {
+                name: "View All Employees By Manager",
+                value: "View_Employee_By_Manager"
+            },
+            {
+                name: "Add Employee",
+                value: "Add_Employee"
+            },
+            {
+                name: "Remove Employee",
+                value: "Remove_Employee"
+            },
+            {
+                name: "Update Employee Role",
+                value: "Update_Employee_ROle"
+            },
+            {
+                name: "Update Employee Manager",
+                value: "Update_Employee_Manager"
+            },
+            {
+                name: "View All Roles",
+                value: "View_Roles"
+            },
+            {
+                name: "Add Role",
+                value: "Add_Role"
+            },
+            {
+                name: "Remove Role",
+                value: "Remove_Role"
+            },
+            {
+                name: "View All Departments",
+                value: "View_Departments"
+            },
+            {
+                name: "Add Department",
+                value: "Add_Department"
+            },
+            {
+                name: "Remove Department",
+                value: "Remove_Department"
+            },
+            {
+                name: "Quit",
+                value: "Quit"
+            }
+        ]
+    }
+])
